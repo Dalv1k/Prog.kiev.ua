4c29967a023f69ecba4a46753e7b2f25ccb34fac
Владислав Кекух <p454@ukr.net>
2016-07-22 12:29:38 +0300

    Допил

--- task323/Wallpapers2.java
+++ task323/Wallpapers2.java
@@ -3,18 +3,14 @@ package net.ukr.p454.task323;
 import java.util.Scanner;
 
 public class Wallpapers2 {
-	/*
-	 * — помощью цикла (только одного) нарисовать такую фигуру. ѕричем
-	 * максимальна€ высота этой фигуры вводитьс€ с клавиатуры (в примере
-	 * максимальна€ высота - 4)
-	 */
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Input the height of the figure:");
 		int height = sc.nextInt();
-		String s = "";
-
+		sc.close();
+		
 		int i = (height * 2) - 1;
 		int counter = 0;
 		int j = 0;
