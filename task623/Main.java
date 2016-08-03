c0c6ceebb0d6992db2dcc555ba52f7baafcab790
Владислав Кекух <p454@ukr.net>
2016-08-03 14:32:34 +0300

    change

--- task623/Main.java
+++ task623/Main.java
@@ -1,6 +1,6 @@
 package net.ukr.p454.task623;
 
-
+import java.awt.Paint;
 import java.util.Scanner;
 
 public class Main {
@@ -13,20 +13,34 @@ public class Main {
 		scanner.close();
 		System.out.println("Min repeat number:" + getRepeatNumber(string));
 	}
-	
+
 	static String getRepeatNumber(String string) {
-		
+
 		String part1 = "";
 		String part2 = "";
 		String min = "";
-		for (int i = string.length()/2; i > 0; i--) {
-			part1 = string.substring(0,i);
-			part2 = string.substring(i, i*2);
-			
-			if (part1.equals(part2)) {
-				min = part1;
+		int counter = 0;
+		for (int i = string.length() / 2; i >0; i--) {
+		
+			for (int j = 0; j <= string.length()-i-i; j = j+i) {
+				
+				part1 = string.substring(j , j+i);
+				part2 = string.substring(j+i,j+i+i); 
+				
+				if (part1.equals(part2)) {
+					counter++;
+				}
+
+				
+				if(counter == (string.length() / i -1) ) {
+					min = part1;
+				}
+				
 			}
+			counter = 0;
+		
 		}
+
 		return min;
 	}
 }
