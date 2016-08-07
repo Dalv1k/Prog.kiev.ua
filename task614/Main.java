36f1812730f20699c3452b7d60d25b397c8fca57
Dalv1k <p454@ukr.net>
2016-08-07 18:58:52 +0300

    add

--- task614/Main.java
+++ task614/Main.java
@@ -7,17 +7,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	printPI(10);	
+		printPI(10);
 	}
-	
-	static void printPI (int count ){
+
+	static void printPI (int count ) {
 		Formatter formatter = new Formatter();
-		
-		for (int i = 2; i <= count +1; i++) {
-			
-			formatter.format("PI = %." + i + "f\n", Math.PI);			
+
+		for (int i = 2; i <= count + 1; i++) {
+			formatter.flush();
+			formatter.format("PI = %." + i + "f\n", Math.PI);
 		}
 		System.out.println(formatter.toString());
+
 	}
 
 }
